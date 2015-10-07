d9116d84a48a9bcb08abef50f2ad6188d137552b
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-07 19:35:57 +0200

    Simplify CLI help creation

--- cli.js
+++ cli.js
@@ -3,16 +3,14 @@
 var meow = require('meow');
 var fn = require('./');
 
-var cli = meow({
-	help: [
-		'Examples',
-		'  $ site-password github.com',
-		'  $ site-password github.com --account example@mail.com',
-		'',
-		'Options',
-		'  -a, --account <account>      Get password for specific account'
-	]
-}, {
+var cli = meow([
+	'Examples',
+	'  $ site-password github.com',
+	'  $ site-password github.com --account example@mail.com',
+	'',
+	'Options',
+	'  -a, --account <account>      Get password for specific account'
+], {
 	string: ['account'],
 	alias: {a: 'account'}
 });
